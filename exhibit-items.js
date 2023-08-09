56350a6c5a021e24513103821687568f65a8163d
Jeremy Boggs <jeremy@virginia.edu>
2023-08-09 15:39:51 -0400

    Get started.

---
+++ exhibit-items.js
@@ -0,0 +1,20 @@
+var exhibit_items = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+        "type":"GeometryCollection",
+        "style": {
+            weight: 2,
+            color: "#999",
+            opacity: 1,
+            fillColor: "#B0DE5C",
+            fillOpacity: 0.8
+        },
+        "geometries":[
+            {
+                "type":"Point","coordinates":[-8697673.0836036, 2063256.1282084]
+            }
+            ]
+        }
+    ]
+}
